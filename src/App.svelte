e36c444404d9a50f99759ea4a1c1ca1c236026a7
Phongsakorn Wisetthon <boy1556@hotmail.com>
2022-08-12 21:48:37 +0700

    test

--- src/App.svelte
+++ src/App.svelte
@@ -1,34 +1,40 @@
 <script>
   //import svelteLogo from './assets/svelte.svg'
   //import Counter from './lib/Counter.svelte'
-  let findwho;
-  let whatyouwant;
+  let findwho,findwhoth,whatyouwant,phonenumber;
   import { Styles,Container,FormGroup,Label,Input,Form } from 'sveltestrap';
 
   function who(player){
+    if(player == 'boy'){
+      whatyouwant = 'express'
+    }else if(player == 'cht'){
+      phonenumber = '0624892914'
+    }else{
+      whatyouwant = ''
+    }
     findwho = player;
     return true;
+  }
+  function myNewFunction(sel) {
+    //alert(sel.options[sel.selectedIndex].text);
+    findwhoth = sel.options[sel.selectedIndex].text;
   }
 </script>
 
 <Styles />
 
-<main>
   <Container sm>
     <h3 class="bg-light">ไม่เจอใครอยู่บ้านหรอ?</h3>
-    <!--Form>
-      <FormGroup-->
         <Label for="exampleSelect">คุณต้องการติดต่อใคร?</Label>
-        <Input type="select" value="def" on:change={(e)=>who(e.target.value)}>
+        <Input type="select" value="def" on:change={(e)=>{who(e.target.value);myNewFunction(e.target);}}>
           <option value="def">เลือกคุณที่ต้องการติดต่อ</option>
           <option value="boy">พงศกร</option>
           <option value="cht">ชัชวาลย์</option>
           <option value="dawn">ดวงจันทร์</option>
+          <option value="pukbung">พัชรธิดา</option>
           <option value="someone">ใครก็ได้ที่ใกล้ที่สุด</option>
-          <!--option>5</option-->
+          <option value="who">ไม่มีชื่ออยู่ในนี้</option>
         </Input>
-      <!--/FormGroup>  
-    </Form-->
     {#if findwho == 'boy'}
       <br>
       <Label for="exampleSelect">ต้องการติดต่อพงศกร เรื่องอะไร?</Label>
@@ -38,12 +44,23 @@
         <option value="cht">ติดต่อให้ทำงาน/ซ่อมคอม/ซ่อมมือถือ</option>
       </Input>
     {/if}
+    {#if findwho == 'cht'}
+      <br>
+      <Label for="exampleSelect">ต้องการติดต่อชัชวาลย์ เรื่องอะไร?</Label>
+      <Input type="select" value="express" on:change={(e)=>whatyouwant=e.target.value}>
+        <option value="contact">ติดต่อ</option>
+        <option value="other">อื่นๆ</option>
+      </Input>
+    {/if}
     {#if whatyouwant == 'express'}
-      ถ้าเป็นของหรือจดหมายที่ต้องจ่ายเงิน กดปุ่มด้านล่างนี้
-      <a href="tel:0801643864" class="btn btn-primary">ติดต่อพงศกร</a>
+      ถ้าเป็นของหรือจดหมายที่ต้องจ่ายเงิน กดปุ่มโทร หรือ เบอร์โทร 080-164-3864
+      <a href="tel:0801643864" class="btn btn-primary">โทร</a>
+    {/if}
+    {#if whatyouwant == 'contact'}
+      ต้องการติดต่อ {findwhoth} กดปุ่มโทร หรือ เบอร์โทร {phonenumber}
+      <a href="tel:{phonenumber}" class="btn btn-primary">โทร</a>
     {/if}
   </Container>
-</main>
 
 <style>
   /*.logo {
